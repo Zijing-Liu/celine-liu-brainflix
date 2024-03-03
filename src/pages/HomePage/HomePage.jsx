dfab6af6d95c1095cab05ec93ccd4924e4d3466c
Celine Liu <mangolzj@gmail.com>
2024-03-03 14:30:10 -0500

    fetch videoList data from api

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,26 +3,36 @@ import MainVideo from "../../components/MainVideo/MainVideo";
 import VideoList from "../../components/VideoList/VideoList";
 import MainVideoInfo from "../../components/MainVideoInfo/MainVideoInfo";
 import { useState, useEffect } from "react";
-import videos from "../../data/videos.json";
 import videoDetail from "../../data/video-details.json";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function HomePage() {
-  const [videoList, setVideoList] = useState(videos);
+  const [videoList, setVideoList] = useState([]);
+  const API_KEY = "d3264812-4975-47da-8eb7-626943222c00";
   // set the default video in play
   //  states to control the updates of activeVideo (video in play)
   const [activeVideo, setActiveVideo] = useState(videoDetail[0]);
   // get the params in url to locate currentVideo (video selected by the user))
   const { videoID } = useParams(); // the whole component will render when it changes
-  console.log(videoID);
   // find the details of the video that match the videoID param
-  // only updates the activeVideo state when the param is a valid video id
-
   // re-render the page when the activeVideo changes
+
   useEffect(() => {
-    if (videoDetail.find((video) => video.id === videoID)) {
-      setActiveVideo(videoDetail.find((video) => video.id === videoID));
-    }
+    const getVideoList = async () => {
+      try {
+        const response = await axios.get(
+          `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/?api_key=${API_KEY}`
+        );
+        setVideoList(response.data);
+        if (videoDetail.find((video) => video.id === videoID)) {
+          setActiveVideo(videoDetail.find((video) => video.id === videoID));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getVideoList();
   }, [videoID]);
 
   return (
